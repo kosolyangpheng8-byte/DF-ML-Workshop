48db9308a31ed8981b7d8b827dd941438e2a88e1
kosolyangpheng8-byte <kosolyangpheng8@gmail.com>
2026-06-10 15:18:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ elif st.button("💰 การพยากรณ์ระยะเวลาก�
     
 elif st.button("💰 จัดกลุ่มข้อมูล"):
     st.switch_page("pages/Clustering_segment.py")
-
+elif st.button("💰 ระบบแนะนำสินค้าจาก Market Basket Analysis"):
+    st.switch_page("pages/association_items.py")
 
 
 
